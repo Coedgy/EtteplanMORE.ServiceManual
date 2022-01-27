6a2c5f988d85d50e7901fc2d7afb0a36e850d50a
Coedgy <samu.salmivaara@gmail.com>
2022-01-27 21:20:17 +0200

    Update MaintenanceTaskDto.cs

--- EtteplanMORE.ServiceManual.Web/Dtos/MaintenanceTaskDto.cs
+++ EtteplanMORE.ServiceManual.Web/Dtos/MaintenanceTaskDto.cs
@@ -1,7 +1,14 @@
-﻿namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
+﻿using System;
+
+namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
 {
     public class MaintenanceTaskDto
     {
-        
+        public int Id { get; set; }
+        public int Device { get; set; }
+        public DateTime IssueDate { get; set; }
+        public string Description { get; set; }
+        public ImportanceLevel Importance { get; set; }
+        public bool Closed { get; set; }
     }
 }
