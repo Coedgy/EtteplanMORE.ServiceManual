492c6439534ff73f046b55086a10eceeecca91fe
Coedgy <samu.salmivaara@gmail.com>
2022-01-27 18:03:09 +0200

    Helper class for ConnectionString method

---
+++ EtteplanMORE.ServiceManual.ApplicationCore/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace EtteplanMORE.ServiceManual.ApplicationCore
+{
+    internal static class Helper
+    {
+        public static string ConnectionString(string connectionName = "Default")
+        {
+            return System.Configuration.ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+    }
+}
