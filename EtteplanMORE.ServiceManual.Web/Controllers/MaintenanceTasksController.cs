6c2007bf2872820950576371b2fa193ddf16565f
Coedgy <samu.salmivaara@gmail.com>
2022-01-27 18:38:57 +0200

    Create MaintenanceTasksController.cs

---
+++ EtteplanMORE.ServiceManual.Web/Controllers/MaintenanceTasksController.cs
@@ -0,0 +1,11 @@
+﻿using EtteplanMORE.ServiceManual.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EtteplanMORE.ServiceManual.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class MaintenanceTasksController : Controller
+    {
+        
+    }
+}
