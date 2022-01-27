7551271c16a284072f24dcc8dab759bbffd31c34
Coedgy <samu.salmivaara@gmail.com>
2022-01-27 18:40:47 +0200

    Create a data transfer object for tasks and add dependency injection to controller

--- EtteplanMORE.ServiceManual.Web/Controllers/MaintenanceTasksController.cs
+++ EtteplanMORE.ServiceManual.Web/Controllers/MaintenanceTasksController.cs
@@ -6,6 +6,11 @@ namespace EtteplanMORE.ServiceManual.Web.Controllers
     [Route("api/[controller]")]
     public class MaintenanceTasksController : Controller
     {
-        
+        private readonly IMaintenanceTaskService _maintenanceTaskService;
+
+        public MaintenanceTasksController(IMaintenanceTaskService maintenanceTaskService)
+        {
+            _maintenanceTaskService = maintenanceTaskService;
+        }
     }
 }
