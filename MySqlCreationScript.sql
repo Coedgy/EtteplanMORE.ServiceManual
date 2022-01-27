7f5613b11054f9eeb122799cb00330b4bbea7ccd
Coedgy <samu.salmivaara@gmail.com>
2022-01-27 17:35:53 +0200

    Create a SQL script for the MySQL database

---
+++ MySqlCreationScript.sql
@@ -0,0 +1,23 @@
+CREATE SCHEMA `esm` ;
+
+CREATE TABLE `esm`.`FactoryDevices` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(68) NOT NULL,
+  `year` INT NOT NULL,
+  `type` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `esm`.`MaintenanceTasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `deviceId` INT NOT NULL,
+  `issueDate` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `description` VARCHAR(200) NULL,
+  `importance` INT NOT NULL DEFAULT 0,
+  `closed` TINYINT NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  INDEX `deviceId_idx` (`deviceId` ASC) VISIBLE,
+  CONSTRAINT `deviceId`
+    FOREIGN KEY (`deviceId`)
+    REFERENCES `esm`.`FactoryDevices` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
