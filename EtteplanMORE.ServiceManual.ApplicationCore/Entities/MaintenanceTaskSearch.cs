32ba746fcf6627f9e3e988939ce30a8d6ad7b37b
Coedgy <samu.salmivaara@gmail.com>
2022-02-02 18:47:20 +0200

    Added a model for searching tasks

---
+++ EtteplanMORE.ServiceManual.ApplicationCore/Entities/MaintenanceTaskSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
+{
+    public class MaintenanceTaskSearch
+    {
+        public int Id { get; set; }
+        public int DeviceId { get; set; }
+        public DateTime IssueDateFrom { get; set; }
+        public DateTime IssueDateTo { get; set; }
+        public string DescriptionIncludes { get; set; }
+        public ImportanceLevel ImportanceMin { get; set; }
+        public ImportanceLevel ImportanceMax { get; set; }
+        public bool? Closed { get; set; }
+    }
+}
